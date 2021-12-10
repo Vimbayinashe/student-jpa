941d67eba0c3f3c66f80d48af8245f84f8b44e5a
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-12-10 17:14:06 +0100

    Add Student DAO

---
+++ src/main/java/org/example/Gender.java
@@ -0,0 +1,3 @@
+package org.example;
+
+public enum Gender { MALE, FEMALE }
---
+++ src/main/java/org/example/dao/StudentDAO.java
@@ -0,0 +1,69 @@
+package org.example.dao;
+
+import org.example.Gender;
+import org.example.student.Student;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public interface StudentDAO {
+
+    void create(Student student);
+
+    void update(Student student);
+
+    void delete(Student student);
+
+    Student getById(int id);
+
+    Student getOutstandingBalance(int id);
+
+    List<String> getProgrammes();
+
+    List<Student> getByName(String name);
+
+    List<Student> getByNameExact(String name);
+
+    List<Student> getByProgramme(String programme);
+
+    List<Student> getByProgrammeAndIsActive(String programme, boolean active);
+
+    List<Student> getAll();
+
+    List<Student> getByAge(int age);
+
+    List<Student> getByAgeInterval(int min, int max);
+
+    List<Student> getByGender(Gender gender);
+
+    List<Student> getByCredits(int credits);
+
+    List<Student> getByCreditsRange(int min, int max);
+
+    List<Student> getByProgrammeAndGender(String programme, Gender gender);
+
+    List<Student> getAllAndIsActive(boolean active);
+
+    List<Student> getAllByProgramAndIsActive(boolean active, String programme); //todo - remove?
+
+
+    Map<String, String> getAllAndGroupByProgram();     //todo - use Map
+
+
+    BigDecimal getTotalOutstandingBalance();
+
+    List<Student> getAllWithOutstandingBalance();
+
+    List<Student> getOutstandingBalanceByProgramme(String programme);
+
+    List<Student> getOutstandingBalanceAndIsActive(boolean active);
+
+
+    Map<String, List<Student>> getOutstandingBalanceAndGroupByProgram();
+
+    Map<String, BigDecimal> getOutstandingBalanceByProgram();
+
+    Map<String, Integer> getOutstandingBalanceCountByProgram(); //order DESC
+
+  }
