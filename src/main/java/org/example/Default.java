e9e0de12ff66270cdb6f8a9d3312769e650e0b1e
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-12-11 12:16:55 +0100

    Add test data

--- src/main/java/org/example/Default.java
+++ src/main/java/org/example/Default.java
@@ -34,8 +34,31 @@ public class Default {
     private static List<List<String>> getStudentDetails() {
         return List.of(
                 List.of("Almir", "Eriksson", "19860712-6979", "0550-4889051", "AlmirEriksson@jourrapide.com",
-                        "Law", "240", "120000", "100000")
-
+                        "Law", "240", "120000", "100000"),
+                List.of("Ebbe", "Olofsson", "19990727-0070", "044-8902518", "EbbeOlofsson@rhyta.com",
+                        "Medicine", "300", "150000", "90000"),
+                List.of("Sonia", "Lund", "20000612-0869", "0651-22590888", "SoniaLund@dayrep.com",
+                        "Medicine", "300", "150000", "90000"),
+                List.of("Alexandra", "Eliasson", "19960131-6945", "0370-3066315", "AlexsandraEliasson@jourrapide.com",
+                        "Medicine", "300", "150000", "140000"),
+                List.of("Gordon", "Bergström", "19980407-5137", "0515-6919619", "GordonBergstrom@jourrapide.com",
+                        "Medicine", "300", "150000", "90000"),
+                List.of("Caspar", "Jonasson", "19961013-4794", "040-3392667", "CasparJonasson@dayrep.com",
+                        "Music", "180", "80000", "60000"),
+                List.of("Angelina", "Martins", "19950206-8082 ", "0393-5343197", "AngelinaEriksson@teleworm.us",
+                        "Software Development", "180", "120000", "110000"),
+                List.of("Albin", "Eriksson", "20030329-5989", "0382-8391709", "AlbinaEriksson@jourrapide.com",
+                        "Software Development", "180", "120000", "0"),
+                List.of("Kevin", "Horst", "19990523-2775 ", "0471-8603325", "KevinIHorst@teleworm.us",
+                        "Music", "180", "80000", "80000"),
+                List.of("Lissa", "Herrera", "19980921-6465", "0645-3425189", "LisaNHerrera@dayrep.com",
+                        "Civil Engineering", "240", "180000", "180000"),
+                List.of("Charles", "Briones", "19990801-7170", "0525-4919893", "CharlesSBriones@rhyta.com",
+                        "Civil Engineering", "240", "180000", "70525"),
+                List.of("Patrik", "McNeal", "19970826-8799", "0973-9520949", "PatrickRMcNeal@dayrep.com",
+                        "Education", "240", "120000", "20500"),
+                List.of("Daniella", "Sandvik", "20021226-6218 ", "0140-7855270", "DonaldABricker@rhyta.com",
+                        "Education", "240", "120000", "90995")
         );
     }
 
@@ -44,3 +67,8 @@ public class Default {
     }
 
 }
+
+
+
+
+
